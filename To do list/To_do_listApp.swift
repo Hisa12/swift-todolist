ad2c81d190ae547a58bbb6b5740e3c7fc5d6b843
Hisa12 <hisago.52612@gmail.com>
2024-06-18 15:18:45 +1000

    Initial Commit

---
+++ To do list/To_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_listApp.swift
+//  To do list
+//
+//  Created by Hisano Sato on 18/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct To_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
